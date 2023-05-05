8af78e8398753cce6e663b788e1f86c55c24c5f7
Marlena Rybak <marlen.pescar@gmail.com>
2023-05-06 00:09:38 +0200

    add views

--- randomthought/views.py
+++ randomthought/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+
+
+THOUGHT_LIST = [
+    'baba z wozu, koniom lżej',
+    'gdzie diabeł nie może, tam babę pośle',
+    'jeszcze się taki nie narodził, co by babie we wszystkim dogodził',
+]
+
+def ThoughtView(request):
+    thought = random.choice(THOUGHT_LIST)
+    context = {'thought': thought}
+    return render(
+        request,
+        'thought.html',
+        context
+    )
+
